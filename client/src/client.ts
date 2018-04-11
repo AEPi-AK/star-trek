43aad1ef4225ba9ddb13fc989384cb46005c89c6
Jordan Brown <jmbrown@andrew.cmu.edu>
2018-04-11 18:27:35 -0400

    added sample subproject

--- client/src/client.ts
+++ client/src/client.ts
@@ -1,6 +1,6 @@
 import Socket = require('socket.io-client');
 
-var socket = Socket('http://localhost:3000');
+var socket = Socket('http://Meins-MacBook-Pro.local:3000');
 
 socket.on('connect', () => {
   socket.emit('identification', 'Command line client');
