f52bca3f8146b6ea1514777f318242dd6a38b7e9
Jordan Brown <jmbrown@andrew.cmu.edu>
2018-04-11 19:20:07 -0400

    Client set to localhost

--- client/src/client.ts
+++ client/src/client.ts
@@ -1,7 +1,10 @@
 import Socket = require('socket.io-client');
 
-var socket = Socket('http://Meins-MacBook-Pro.local:3000');
+var socket = Socket('http://localhost:3000');
 
 socket.on('connect', () => {
   socket.emit('identification', 'Command line client');
 });
+
+//@ts-ignore
+socket.on('TEST', dat => console.log(dat));
