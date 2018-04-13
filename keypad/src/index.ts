379e94010c1a43a9597fba7e7c42eb4ed47c9d50
Jonathan Merrin <Jonmerrin@gmail.com>
2018-04-12 21:44:10 -0400

    fixing some things with the rfid and keypad controls

--- keypad/src/index.ts
+++ keypad/src/index.ts
@@ -1,14 +1,32 @@
 import Socket = require('socket.io-client');
 import rpio = require('rpio');
-import iohook = require('iohook');
+import readline = require('readline');
+
+
+//import iohook = require('iohook');
 console.log("Keypad started.");
 
-function keyPress(event : KeyboardEvent){
-    console.log(event);
-};
+readline.emitKeypressEvents(process.stdin);
+
+//@ts-ignore
+process.stdin.setRawMode(true);
+process.stdin.on('keypress', (str, key) => {
+  if (key.ctrl && key.name === 'c') {
+    process.exit();
+  } else {
+    console.log(`You pressed the "${str}" key`);
+    console.log();
+    console.log(key);
+    console.log();
+  }
+});
+
+//process.stdin.on('keypress', (ch, key) => {console.log("keypress found", ch);});
 
 var socket = Socket('http://localhost:3000');
 
+
+/*
 var translations = {
     71: 7,
     72: 8,
@@ -32,6 +50,23 @@ socket.on('PLAYERS', () => {
             //@ts-ignore
             socket.emit('players', translations[event.keycode])
             iohook.stop();
+        }
+    });
+});
+*/
+//@ts-ignore
+socket.on('PLAYERS', () => {
+    //@ts-ignore
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
+    console.log("How many players are there?");
+    process.stdin.on('keypress', (ch,key) => {
+        console.log(ch);
+        if(!isNaN(ch)){
+            //@ts-ignore
+            socket.emit('players', parseInt(ch));
+            process.stdin.removeAllListeners('keypress');
+            //@ts-ignore
         }
     });
 });
