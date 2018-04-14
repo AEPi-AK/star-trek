1c4e4e0e6642620b8dbd110c80c57fa0f1331417
Jonathan Merrin <Jonmerrin@gmail.com>
2018-04-14 15:46:18 -0400

    now with 'stop listening' functionality, and checking if the sequence has been found

--- keypad/src/Keypad.ts
+++ keypad/src/Keypad.ts
@@ -1,13 +1,13 @@
 import { Device, getDeviceList, on as onUsb, InEndpoint } from 'usb';
 
 const VENDOR_ID = 3141; // 2^16 - 1. Lol. Very lazy of them.
-function deviceIsCardKeypad(device: Device): boolean {
+function deviceIsKeypad(device: Device): boolean {
   return device.deviceDescriptor.idVendor === VENDOR_ID;
 }
 
 
 function watchDevice(device: Device, sendPacket: (p: any) => any): void {
-  if (!deviceIsCardKeypad(device)) {
+  if (!deviceIsKeypad(device)) {
     return;
   }
   console.log(`Keypad connected`);
@@ -31,7 +31,20 @@ function watchDevice(device: Device, sendPacket: (p: any) => any): void {
 
   (endpoint as InEndpoint).startPoll(1, 4);
 
-  let scanCodes: number[] = [];
+  var keyMap = {
+    89: 1,
+    90: 2,
+    91: 3,
+    92: 4,
+    93: 5,
+    94: 6,
+    95: 7,
+    96: 8,
+    97: 9,
+    98: 0,
+  }
+
+
 
   endpoint.on('data', (data: Buffer) => {
     console.log("Buffer: ", (data.toString('hex', 2, 4)));
@@ -43,14 +56,11 @@ function watchDevice(device: Device, sendPacket: (p: any) => any): void {
       // Only push numbers 0-9
       // https://github.com/abcminiuser/lufa/blob/master/LUFA/Drivers/USB/Class/Common/HIDClassCommon.h#L113
       console.log(`Read keypress with value: ${scanCode}`);
-      /*const entry = manifest.find(m => m.sequence === sequence);
-      if (entry) {
-        console.log(`Identified ${entry.name} (id ${entry.cardID})`);
         sendPacket({
-          kind: 'scan',
-          cardID: entry.cardID,
+          kind: 'keypad',
+          //@ts-ignore
+          key: keyMap[scanCode],
         });
-      }*/
       }
   });
 
@@ -59,12 +69,44 @@ function watchDevice(device: Device, sendPacket: (p: any) => any): void {
   });
 }
 
+function unwatchDevice(device : Device){
+  if (!deviceIsKeypad(device)) {
+    return;
+  }
+  console.log(`Keypad connected`);
+  device.open();
+  const iface = device.interfaces[0];
+
+  // The RFID reader is recognized as a keyboard when plugged
+  // in. So we want to tell the kernel to stop treating it as such, and
+  // let us have full control.
+  if (iface.isKernelDriverActive()) {
+    iface.detachKernelDriver();
+  }
+
+  iface.claim();
+
+  const endpoint = iface.endpoints[0];
+
+  if (endpoint.direction !== 'in') {
+    throw Error('invalid endpoint for interface');
+  }
+
+  (endpoint as InEndpoint).removeAllListeners();
+}
+
 export default class Keypad {
   constructor(sendPacket: (p: any) => any) {
     getDeviceList()
-      .filter(deviceIsCardKeypad)
+      .filter(deviceIsKeypad)
       .forEach(device => watchDevice(device, sendPacket.bind(this)));
 
     onUsb('attach', device => watchDevice(device, sendPacket.bind(this)));
+  }
+
+  stopWatching(){
+    getDeviceList()
+    .filter(deviceIsKeypad)
+    .forEach(device => unwatchDevice(device));
   }
 }
--- keypad/src/index.ts
+++ keypad/src/index.ts
@@ -11,11 +11,18 @@ console.log("Keypad started.");
 
 var socket: SocketIOClient.Socket = Socket('http://localhost:3000');
 
+const sequence:number[] = [];
 
 socket.on('code', (target : string) => {
         function sendPacket (packet : any) {
             console.log(packet);
-            socket.emit('task-completed', 98);
+            sequence.shift();
+            sequence.push(packet.key);
+            if(sequence.join("") == target){
+                socket.emit('task-completed', 98);
+            } else {
+                console.log("Not yet. Currently, the sequence is", sequence)
+            }
         }
         
         const keypad = new Keypad(sendPacket);
