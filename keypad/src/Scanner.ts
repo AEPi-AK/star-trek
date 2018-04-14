7ea04200b2604c997d8c88fa980992106f6204f7
Jonathan Merrin <Jonmerrin@gmail.com>
2018-04-13 23:52:34 -0400

    testing buttons

---
+++ keypad/src/Scanner.ts
@@ -0,0 +1,75 @@
+import { Device, getDeviceList, on as onUsb, InEndpoint } from 'usb';
+
+const VENDOR_ID = 65535; // 2^16 - 1. Lol. Very lazy of them.
+function deviceIsCardScanner(device: Device): boolean {
+  return device.deviceDescriptor.idVendor === VENDOR_ID;
+}
+
+
+function watchDevice(device: Device, sendPacket: (p: any) => any): void {
+  if (!deviceIsCardScanner(device)) {
+    return;
+  }
+  console.log(`Scanner connected`);
+  device.open();
+  const iface = device.interfaces[0];
+
+  // The RFID reader is recognized as a keyboard when plugged
+  // in. So we want to tell the kernel to stop treating it as such, and
+  // let us have full control.
+  if (iface.isKernelDriverActive()) {
+    iface.detachKernelDriver();
+  }
+
+  iface.claim();
+
+  const endpoint = iface.endpoints[0];
+
+  if (endpoint.direction !== 'in') {
+    throw Error('invalid endpoint for interface');
+  }
+
+  (endpoint as InEndpoint).startPoll(1, 1);
+
+  let scanCodes: number[] = [];
+
+  endpoint.on('data', (data: Buffer) => {
+    const scanCode = parseInt(data.toString('hex', 2, 3), 16);
+    // Every other scan code is blank padding
+    if (scanCode === 0) {
+      return;
+    } else if (scanCode >= 0x1e && scanCode <= 0x27) {
+      // Only push numbers 0-9
+      // https://github.com/abcminiuser/lufa/blob/master/LUFA/Drivers/USB/Class/Common/HIDClassCommon.h#L113
+      scanCodes.push(scanCode - 0x1d);
+    } else if (scanCode === 0x28) {
+      // If the enter key was pressed
+      const sequence = Number(scanCodes.join(''));
+      console.log(`Read card with sequence: ${sequence}`);
+      /*const entry = manifest.find(m => m.sequence === sequence);
+      if (entry) {
+        console.log(`Identified ${entry.name} (id ${entry.cardID})`);
+        sendPacket({
+          kind: 'scan',
+          cardID: entry.cardID,
+        });
+      }*/
+
+      scanCodes = [];
+    }
+  });
+
+  endpoint.on('error', error => {
+    console.log(`Scanner disconnected`);
+  });
+}
+
+export default class Scanner {
+  constructor(sendPacket: (p: any) => any) {
+    getDeviceList()
+      .filter(deviceIsCardScanner)
+      .forEach(device => watchDevice(device, sendPacket.bind(this)));
+
+    onUsb('attach', device => watchDevice(device, sendPacket.bind(this)));
+  }
+}
--- keypad/src/index.ts
+++ keypad/src/index.ts
@@ -1,70 +1,22 @@
 import Socket = require('socket.io-client');
 import rpio = require('rpio');
 import readline = require('readline');
+import Keypad from "./Keypad";
 
 
 //import iohook = require('iohook');
 
 console.log("Keypad started.");
 
-readline.emitKeypressEvents(process.stdin);
-
-//@ts-ignore
-process.stdin.setRawMode(true);
-process.stdin.on('keypress', (str, key) => {
-  if (key.ctrl && key.name === 'c') {
-    process.exit();
-  } else {
-    console.log(`You pressed the "${str}" key`);
-    console.log();
-    console.log(key);
-    console.log();
-  }
-});
-
-//process.stdin.on('keypress', (ch, key) => {console.log("keypress found", ch);});
 
 var socket: SocketIOClient.Socket = Socket('http://localhost:3000');
 
 
-/*
-var translations = {
-    71: 7,
-    72: 8,
-    73: 9,
-    75: 4,
-    76: 5,
-    77: 6,
-    79: 1,
-    80: 2,
-    81: 3,
-};
-
-socket.on('players', () => {
-    iohook.start(false);
-    console.log("How many players are there?");
-    iohook.on('keydown', event => {
-        console.log(event.keycode);
-        if(translations[event.keycode] != null){
-            socket.emit('players', translations[event.keycode])
-            iohook.stop();
+socket.on('code', (target : string) => {
+        function sendPacket (packet : any) {
+            console.log(packet);
+            socket.emit('task-completed', 98);
         }
+        
+        const keypad = new Keypad(sendPacket);
     });
-});
-*/
-//@ts-ignore
-socket.on('PLAYERS', () => {
-    //@ts-ignore
-    process.stdin.setRawMode(true);
-    process.stdin.resume();
-    console.log("How many players are there?");
-    process.stdin.on('keypress', (ch,key) => {
-        console.log(ch);
-        if(!isNaN(ch)){
-            //@ts-ignore
-            socket.emit('players', parseInt(ch));
-            process.stdin.removeAllListeners('keypress');
-            //@ts-ignore
-        }
-    });
-});
