16b507f151c7ad087a6f8df8624846b336a334b6
Jonathan Merrin <Jonmerrin@gmail.com>
2018-04-13 21:41:22 -0400

    updating rfid reader

--- rfid-reader/src/index.ts
+++ rfid-reader/src/index.ts
@@ -1,6 +1,7 @@
 import Socket = require('socket.io-client');
 import rpio = require('rpio');
 import readline = require('readline');
+import Scanner from './Scanner';
 
 console.log("rfid started.");
 
@@ -16,14 +17,25 @@ socket.on('rfid', (target: string) => {
         output: process.stdout
     });
     
-    rl.on('line', (input) => {
-        if(input == target){
-            console.log("matched!");
-            socket.emit('rfid-match', true);
-            rl.close();
-        }
+    // rl.on('line', (input) => {
+    //     if(input == target){
+    //         console.log("matched!");
+    //         socket.emit('rfid-match', true);
+    //         rl.close();
+    //     }
 
-    });
+    // });
+
+        
+    function sendPacket (packet : any) {
+        if(packet == target){
+            console.log("matched!");
+            socket.emit('task-completed', 99);
+        } else {
+            console.log("Received, not a match");
+        }
+    }
+
+    const scanner = new Scanner(sendPacket);
 
 });
-
