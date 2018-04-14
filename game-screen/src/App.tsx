4d865516419f726f510a40f090d258073d939d94
Avi Romanoff <avi@romanoff.me>
2018-04-14 16:11:36 -0400

    UI tweaks

--- game-screen/src/App.tsx
+++ game-screen/src/App.tsx
@@ -8,11 +8,11 @@ import { Line } from 'rc-progress';
 var socket: SocketIOClient.Socket = Socket('http://localhost:3000');
 
 var defaultGameState: GameTypes.GameState = {
-  tasks: [{description: 'test', id: 0, time_created: 0, time_expires: 0}],
+  tasks: [{ description: 'test', id: 0, time_created: 0, time_expires: 0 }],
   failures: 0,
   time: 0,
   phase: GameTypes.GamePhase.NotConnected,
-  weights: {}, 
+  weights: {},
   durations: {},
   task_frequency: 0,
 };
@@ -26,11 +26,9 @@ function subscribeToGameState(setTasks: (state: GameTypes.GameState) => void) {
   socket.on('game-state-updated', (t: GameTypes.GameState) => {
     setTasks(t);
   });
-
 }
 
 class App extends React.Component<{}, GameTypes.GameState> {
-
   setGameState(gamestate: GameTypes.GameState) {
     this.setState(gamestate);
   }
@@ -79,92 +77,250 @@ class App extends React.Component<{}, GameTypes.GameState> {
   }
 
   render() {
-
     var probabilityControls = (
       <div>
         Probability Controls and Duration Controls:
         <div>
-          {'Press Button: ' + this.state.weights[GameTypes.TaskType.PressButton]}
-          <button onClick={e => this.incrementProbability(GameTypes.TaskType.PressButton)}>+</button>
-          <button onClick={e => this.decrementProbability(GameTypes.TaskType.PressButton)}>-</button>
+          {'Press Button: ' +
+            this.state.weights[GameTypes.TaskType.PressButton]}
+          <button
+            onClick={e =>
+              this.incrementProbability(GameTypes.TaskType.PressButton)
+            }
+          >
+            +
+          </button>
+          <button
+            onClick={e =>
+              this.decrementProbability(GameTypes.TaskType.PressButton)
+            }
+          >
+            -
+          </button>
           {'Duration: ' + this.state.durations[GameTypes.TaskType.PressButton]}
-          <button onClick={e => this.incrementDuration(GameTypes.TaskType.PressButton)}>+</button>
-          <button onClick={e => this.decrementDuration(GameTypes.TaskType.PressButton)}>-</button>
+          <button
+            onClick={e =>
+              this.incrementDuration(GameTypes.TaskType.PressButton)
+            }
+          >
+            +
+          </button>
+          <button
+            onClick={e =>
+              this.decrementDuration(GameTypes.TaskType.PressButton)
+            }
+          >
+            -
+          </button>
         </div>
         <div>
           {'Scan Hand: ' + this.state.weights[GameTypes.TaskType.ScanHand]}
-          <button onClick={e => this.incrementProbability(GameTypes.TaskType.ScanHand)}>+</button>
-          <button onClick={e => this.decrementProbability(GameTypes.TaskType.ScanHand)}>-</button>
+          <button
+            onClick={e =>
+              this.incrementProbability(GameTypes.TaskType.ScanHand)
+            }
+          >
+            +
+          </button>
+          <button
+            onClick={e =>
+              this.decrementProbability(GameTypes.TaskType.ScanHand)
+            }
+          >
+            -
+          </button>
           {'Duration: ' + this.state.durations[GameTypes.TaskType.ScanHand]}
-          <button onClick={e => this.incrementDuration(GameTypes.TaskType.ScanHand)}>+</button>
-          <button onClick={e => this.decrementDuration(GameTypes.TaskType.ScanHand)}>-</button>
+          <button
+            onClick={e => this.incrementDuration(GameTypes.TaskType.ScanHand)}
+          >
+            +
+          </button>
+          <button
+            onClick={e => this.decrementDuration(GameTypes.TaskType.ScanHand)}
+          >
+            -
+          </button>
         </div>
         <div>
-          {'Flip Switches: ' + this.state.weights[GameTypes.TaskType.FlipSwitches]}
-          <button onClick={e => this.incrementProbability(GameTypes.TaskType.FlipSwitches)}>+</button>
-          <button onClick={e => this.decrementProbability(GameTypes.TaskType.FlipSwitches)}>-</button>
+          {'Flip Switches: ' +
+            this.state.weights[GameTypes.TaskType.FlipSwitches]}
+          <button
+            onClick={e =>
+              this.incrementProbability(GameTypes.TaskType.FlipSwitches)
+            }
+          >
+            +
+          </button>
+          <button
+            onClick={e =>
+              this.decrementProbability(GameTypes.TaskType.FlipSwitches)
+            }
+          >
+            -
+          </button>
           {'Duration: ' + this.state.durations[GameTypes.TaskType.FlipSwitches]}
-          <button onClick={e => this.incrementDuration(GameTypes.TaskType.FlipSwitches)}>+</button>
-          <button onClick={e => this.decrementDuration(GameTypes.TaskType.FlipSwitches)}>-</button>
+          <button
+            onClick={e =>
+              this.incrementDuration(GameTypes.TaskType.FlipSwitches)
+            }
+          >
+            +
+          </button>
+          <button
+            onClick={e =>
+              this.decrementDuration(GameTypes.TaskType.FlipSwitches)
+            }
+          >
+            -
+          </button>
         </div>
         <div>
           {'Plugboard: ' + this.state.weights[GameTypes.TaskType.Plugboard]}
-          <button onClick={e => this.incrementProbability(GameTypes.TaskType.Plugboard)}>+</button>
-          <button onClick={e => this.decrementProbability(GameTypes.TaskType.Plugboard)}>-</button>
+          <button
+            onClick={e =>
+              this.incrementProbability(GameTypes.TaskType.Plugboard)
+            }
+          >
+            +
+          </button>
+          <button
+            onClick={e =>
+              this.decrementProbability(GameTypes.TaskType.Plugboard)
+            }
+          >
+            -
+          </button>
           {'Duration: ' + this.state.durations[GameTypes.TaskType.Plugboard]}
-          <button onClick={e => this.incrementDuration(GameTypes.TaskType.Plugboard)}>+</button>
-          <button onClick={e => this.decrementDuration(GameTypes.TaskType.Plugboard)}>-</button>
+          <button
+            onClick={e => this.incrementDuration(GameTypes.TaskType.Plugboard)}
+          >
+            +
+          </button>
+          <button
+            onClick={e => this.decrementDuration(GameTypes.TaskType.Plugboard)}
+          >
+            -
+          </button>
         </div>
         <div>
           {'Read Code: ' + this.state.weights[GameTypes.TaskType.ReadCode]}
-          <button onClick={e => this.incrementProbability(GameTypes.TaskType.ReadCode)}>+</button>
-          <button onClick={e => this.decrementProbability(GameTypes.TaskType.ReadCode)}>-</button>
+          <button
+            onClick={e =>
+              this.incrementProbability(GameTypes.TaskType.ReadCode)
+            }
+          >
+            +
+          </button>
+          <button
+            onClick={e =>
+              this.decrementProbability(GameTypes.TaskType.ReadCode)
+            }
+          >
+            -
+          </button>
           {'Duration: ' + this.state.durations[GameTypes.TaskType.ReadCode]}
-          <button onClick={e => this.incrementDuration(GameTypes.TaskType.ReadCode)}>+</button>
-          <button onClick={e => this.decrementDuration(GameTypes.TaskType.ReadCode)}>-</button>
+          <button
+            onClick={e => this.incrementDuration(GameTypes.TaskType.ReadCode)}
+          >
+            +
+          </button>
+          <button
+            onClick={e => this.decrementDuration(GameTypes.TaskType.ReadCode)}
+          >
+            -
+          </button>
         </div>
         <div>
           {'Scan Card: ' + this.state.weights[GameTypes.TaskType.ScanCard]}
-          <button onClick={e => this.incrementProbability(GameTypes.TaskType.ScanCard)}>+</button>
-          <button onClick={e => this.decrementProbability(GameTypes.TaskType.ScanCard)}>-</button>
+          <button
+            onClick={e =>
+              this.incrementProbability(GameTypes.TaskType.ScanCard)
+            }
+          >
+            +
+          </button>
+          <button
+            onClick={e =>
+              this.decrementProbability(GameTypes.TaskType.ScanCard)
+            }
+          >
+            -
+          </button>
           {'Duration: ' + this.state.durations[GameTypes.TaskType.ScanCard]}
-          <button onClick={e => this.incrementDuration(GameTypes.TaskType.ScanCard)}>+</button>
-          <button onClick={e => this.decrementDuration(GameTypes.TaskType.ScanCard)}>-</button>
+          <button
+            onClick={e => this.incrementDuration(GameTypes.TaskType.ScanCard)}
+          >
+            +
+          </button>
+          <button
+            onClick={e => this.decrementDuration(GameTypes.TaskType.ScanCard)}
+          >
+            -
+          </button>
         </div>
         <div>
-          {'Press Big Red Button: ' + this.state.weights[GameTypes.TaskType.PressBigButton]}
-          <button onClick={e => this.incrementProbability(GameTypes.TaskType.PressBigButton)}>+</button>
-          <button onClick={e => this.decrementProbability(GameTypes.TaskType.PressBigButton)}>-</button>
-          {'Duration: ' + this.state.durations[GameTypes.TaskType.PressBigButton]}
-          <button onClick={e => this.incrementDuration(GameTypes.TaskType.PressBigButton)}>+</button>
-          <button onClick={e => this.decrementDuration(GameTypes.TaskType.PressBigButton)}>-</button>
+          {'Press Big Red Button: ' +
+            this.state.weights[GameTypes.TaskType.PressBigButton]}
+          <button
+            onClick={e =>
+              this.incrementProbability(GameTypes.TaskType.PressBigButton)
+            }
+          >
+            +
+          </button>
+          <button
+            onClick={e =>
+              this.decrementProbability(GameTypes.TaskType.PressBigButton)
+            }
+          >
+            -
+          </button>
+          {'Duration: ' +
+            this.state.durations[GameTypes.TaskType.PressBigButton]}
+          <button
+            onClick={e =>
+              this.incrementDuration(GameTypes.TaskType.PressBigButton)
+            }
+          >
+            +
+          </button>
+          <button
+            onClick={e =>
+              this.decrementDuration(GameTypes.TaskType.PressBigButton)
+            }
+          >
+            -
+          </button>
         </div>
       </div>
     );
 
-    var tasks = this.state.tasks.map((task) => {
-      var now = (new Date()).getTime();
+    var tasks = this.state.tasks.map(task => {
+      var now = new Date().getTime();
       return (
         <li key={task.id}>
-          <button onClick={event => this.completeTask(task.id)}>
-            {task.description + ': ' + 
-            (Math.max(0, Math.floor((task.time_expires - now) / 1000))).toString()}
+          <button
+            className="Task"
+            onClick={event => this.completeTask(task.id)}
+          >
+            {task.description +
+              ': ' +
+              Math.max(
+                0,
+                Math.floor((task.time_expires - now) / 1000),
+              ).toString()}
           </button>
         </li>
       );
     });
     if (this.state.phase === GameTypes.GamePhase.NotConnected) {
-      return (
-        <div>
-          Not connected to master-server
-        </div>
-      );
+      return <div>Not connected to master-server</div>;
     } else if (this.state.phase === GameTypes.GamePhase.EnterPlayers) {
       return (
         <div>
           <div>
             Enter Players:
-          <button onClick={event => this.addPlayers(1)}>1</button>
+            <button onClick={event => this.addPlayers(1)}>1</button>
             <button onClick={event => this.addPlayers(2)}>2</button>
             <button onClick={event => this.addPlayers(3)}>3</button>
             <button onClick={event => this.addPlayers(4)}>4</button>
@@ -186,20 +342,23 @@ class App extends React.Component<{}, GameTypes.GameState> {
     }
     return (
       <div className="App">
+        <div className="RoundInfo">{'Strikes: ' + this.state.failures}</div>
         <div id="tasks">
           <ul>{tasks}</ul>
         </div>
-        <div white-space="pre">
-          <pre>
-            {'Strikes: ' + this.state.failures + '     ' +
-              'Time between tasks: ' + this.state.task_frequency}
-          </pre>
+        <div className="LineLabel">Time Remaining</div>
+        <Line
+          percent={Math.max(0, this.state.time / 150 * 100)}
+          strokeWidth="3"
+          strokeColor="#008000"
+        />
+        <p>{'Time remaining: ' + Math.max(0, this.state.time)}</p>
+        <button onClick={this.resetGame}>Reset Game</button>
+        <div>
+          {'Time between tasks: ' + this.state.task_frequency}
           <button onClick={e => this.incrementFrequency()}>+</button>
           <button onClick={e => this.decrementFrequency()}>-</button>
         </div>
-        <Line percent={Math.max(0, (this.state.time / 150) * 100)} strokeWidth="3" strokeColor="#008000" />
-        <p>{'Time remaining: ' + Math.max(0, this.state.time)}</p>
-        <button onClick={this.resetGame}>Reset Game</button>
         {probabilityControls}
       </div>
     );
