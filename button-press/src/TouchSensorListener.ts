e0ee19db48604faef24b32b1217e02d45ab45d2f
Jordan Brown <jmbrown@andrew.cmu.edu>
2018-04-16 02:54:27 -0400

    Fix touch sensor

--- button-press/src/TouchSensorListener.ts
+++ button-press/src/TouchSensorListener.ts
@@ -20,25 +20,35 @@ export class TouchSensorListener {
         rpio.open(this.port, rpio.INPUT, rpio.PULL_UP);
         this.old_state = rpio.read(this.port);
 
-        rpio.poll(this.port, (pin : number) => {
-            rpio.msleep(10);
-            var new_state : number = rpio.read(pin);
-            if (new_state !== this.old_state) {
-                this.old_state = new_state;
-                var state = {
-                    label: this.label,
-                    lit: false,
-                    pressed: this.old_state ? false : true,
-                };
-                let now = new Date();
-                if (now.getSeconds() - this.lastOff.getSeconds() >= 1) {
-                    console.log(this.label + "has been pressed, new state %d", new_state);
-                    this.socket.emit('button-pressed', state);
+        let on_count = 0;
+        setInterval(() => {
+            if (rpio.read(this.port) === rpio.HIGH) {
+                on_count++;
+                if (on_count === 10) {
+                    console.log('touchpad pressed');
+                    this.socket.emit('button-pressed', {label: this.label, lit: false, pressed: false});
                 }
-                if (new_state == 0) {
-                    this.lastOff = now;
+            } else {
+                if (on_count >= 10) {
+                    console.log('touchpad unpressed');
+                    this.socket.emit('button-pressed', {label: this.label, lit: false, pressed: true})
                 }
+                on_count = 0;
             }
-        });
+        }, 100);
+        // rpio.poll(this.port, (pin : number) => {
+        //     rpio.msleep(10);
+        //     var new_state : number = rpio.read(pin);
+        //     this.old_state = new_state;
+        //     let now = new Date();
+        //     if (now.getSeconds() - this.lastOff.getSeconds() >= 1 && new_state === 1) {
+        //         console.log(this.label + "has been pressed, new state %d", new_state);
+        //         this.socket.emit('button-pressed', state);
+        //     }
+        //     if (new_state === 0) {
+        //         this.lastOff = now;
+        //     }
+
+        // });
     }
 }
