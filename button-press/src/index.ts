33249f3aaf7027b16b5ca23da75d3261d7396927
Jordan Brown <jmbrown@andrew.cmu.edu>
2018-04-15 22:02:02 -0400

    Captains chair flash

--- button-press/src/index.ts
+++ button-press/src/index.ts
@@ -210,8 +210,9 @@ else if (process.argv[3] === 'stationD') {
 }
 else if (process.argv[3] === 'captains-chair') {
     console.log('starting captains chair');
-    let redButton = new PullUpListener(5, "big-red-button");
+    let redButton = new PullUpListener(5, "big-red-button", 10);
     redButton.init();
+    redButton.flash();
 
     var keypad = new KeypadListener(socket);
 
