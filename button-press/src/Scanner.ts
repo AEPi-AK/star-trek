92e79fbbc6848e00b808cab6729772bf110867d3
Jordan Brown <jmbrown@andrew.cmu.edu>
2018-04-15 23:38:45 -0400

    ??? PLS

--- button-press/src/Scanner.ts
+++ button-press/src/Scanner.ts
@@ -31,7 +31,7 @@ function watchDevice(device: Device, sendPacket: (p: any) => any): void {
 
   (endpoint as InEndpoint).startPoll(1, 8);
 
-  let scanCodes: number[] = [];
+  var scanCodes: number[] = [];
 
   endpoint.on('data', (data: Buffer) => {
     const scanCode = parseInt(data.toString('hex', 2, 3), 16);
@@ -41,6 +41,7 @@ function watchDevice(device: Device, sendPacket: (p: any) => any): void {
       return;
     } else if (scanCode >= 0x1e && scanCode <= 0x27) {
       console.log('valid scan number');
+      console.log(scanCode);
       // Only push numbers 0-9
       // https://github.com/abcminiuser/lufa/blob/master/LUFA/Drivers/USB/Class/Common/HIDClassCommon.h#L113
       scanCodes.push(scanCode - 0x1d);
