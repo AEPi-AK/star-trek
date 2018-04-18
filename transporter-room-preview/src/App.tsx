7aa279978a5e34bbaf0f893fef93dc508da60e46
Caroline Hermans <carooolinehermans@gmail.com>
2018-04-17 20:53:54 -0400

    add countdown timer for getting your photo taken

--- transporter-room-preview/src/App.tsx
+++ transporter-room-preview/src/App.tsx
@@ -4,12 +4,16 @@ import * as Socket from 'socket.io-client';
 
 var socket = Socket('http://localhost:5000');
 
-class App extends React.Component<{}, { phase: number; percentDone: number }> {
+class App extends React.Component<
+  {},
+  { phase: number; percentDone: number; timer: number }
+> {
   constructor(props: {}) {
     super(props);
     this.state = {
       phase: 0,
-      percentDone: 0
+      percentDone: 0,
+      timer: 3
     };
   }
 
@@ -21,12 +25,12 @@ class App extends React.Component<{}, { phase: number; percentDone: number }> {
     if (this.state.phase === 0) {
       return (
         <div>
-          {/* <object
-                id="video"
-                type="text/html"
-                data="http://localhost:5000/video_feed"
-            /> */}
-          <img id="video" src="img/webcam.png" />
+          <object
+            id="video"
+            type="text/html"
+            data="http://localhost:5000/video_feed"
+          />
+          {/* <img id="video" src="img/webcam.png" /> */}
           <img id="foreground" src="img/foreground.png" />
           {/* <img id="capture-button" src="img/beam-up.png" onClick={e => this.capture()}/> */}
           <img
@@ -35,14 +39,25 @@ class App extends React.Component<{}, { phase: number; percentDone: number }> {
             onClick={() => {
               const webcamStartTime = Date.now();
               const interval = window.setInterval(() => {
-                console.log('yo');
                 this.setState({
-                  percentDone: (Date.now() - webcamStartTime) / 1000,
+                  percentDone: (Date.now() - webcamStartTime) / 5000,
                   phase: 1
                 });
                 if (this.state.percentDone >= 1) {
                   window.clearInterval(interval);
-                  this.setState({ phase: 2 });
+                  this.setState({ phase: 1.5 });
+                  const timerStartTime = Date.now();
+                  const interval2 = window.setInterval(() => {
+                    this.setState({
+                      timer: Math.round(
+                        3 - (Date.now() - timerStartTime) / 1000
+                      )
+                    });
+                    if (this.state.timer <= 0) {
+                      this.setState({ phase: 2, timer: 3 });
+                      window.clearInterval(interval2);
+                    }
+                  }, 1000);
                 }
               }, 10);
             }}
@@ -53,7 +68,12 @@ class App extends React.Component<{}, { phase: number; percentDone: number }> {
       const beamBarWidth = 460;
       return (
         <div>
-          <img id="video" src="img/webcam.png" />
+          <object
+            id="video"
+            type="text/html"
+            data="http://localhost:5000/video_feed"
+          />
+          {/* <img id="video" src="img/webcam.png" /> */}
           <img id="foreground" src="img/foreground.png" />
           <div id="beam-text">BEAMING</div> <div id="beam-background" />
           <div
@@ -62,13 +82,31 @@ class App extends React.Component<{}, { phase: number; percentDone: number }> {
           />
         </div>
       );
+    } else if (this.state.phase === 1.5) {
+      return (
+        <div>
+          <object
+            id="video"
+            type="text/html"
+            data="http://localhost:5000/video_feed"
+          />
+          {}
+          <div id="countdown-text">{this.state.timer}</div>
+          <img id="foreground" src="img/foreground.png" />
+        </div>
+      );
     } else if (this.state.phase === 2) {
       setTimeout(() => {
         this.setState({ phase: 3 });
       }, 400);
       return (
         <div>
-          <img id="video" src="img/webcam.png" />
+          <object
+            id="video"
+            type="text/html"
+            data="http://localhost:5000/video_feed"
+          />
+          {}
           <div id="flash" />
           <img id="foreground" src="img/foreground.png" />
         </div>
@@ -76,6 +114,12 @@ class App extends React.Component<{}, { phase: number; percentDone: number }> {
     } else {
       return (
         <div>
+          <object
+            id="video"
+            type="text/html"
+            data="http://localhost:5000/video_feed"
+            style={{ maxWidth: '1px' }}
+          />
           <img id="photo" src="img/webcam.png" />
           <img id="foreground" src="img/foreground.png" />
           <div id="end-text">
